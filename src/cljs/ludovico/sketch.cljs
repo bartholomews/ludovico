ad66b39b521e50ec8e2698bc838829ffe6d7549a
bartholomews <github@bartholomews.io>
2020-11-12 22:41:01 +0000

    clean-up

--- src/cljs/ludovico/sketch.cljs
+++ src/cljs/ludovico/sketch.cljs
@@ -70,22 +70,21 @@
   (let [
         distance (note-distance note state)
         percentage (* (/ distance 5.0) 100)
-        pitch-midi-number (j/get note :midi)                ; MIDI pitch number
+        pitch-midi-number (j/get note :midi)
         reverse-percentage (- 100 percentage)
         tile-y (* 500 (/ reverse-percentage 100))
-        note-duration-ms (j/get note :duration)             ; duration ms
-        ;adjusted-res (- res 50)
+        note-duration-ms (j/get note :duration)
         ]
-    (js/console.log "Distance:")
-    (js/console.log distance)
-    (js/console.log "Should display at height:")
-    (js/console.log tile-y)
+    ; FIXME: if tile-y > height should already been filtered out in the take-while
+    ;(js/console.log "Distance:")
+    ;(js/console.log distance)
+    ;(js/console.log "Should display at height:")
+    ;(js/console.log tile-y)
     (q/rect (tile-x pitch-midi-number) tile-y tile-width (tile-height note-duration-ms))
     )
   )
 
 (defn should-display-note [note state]
-  ; notes-to-display (take-while (fn [note] (should-display-note note current-time) notes))
   "Return true if note rect should be be in canvas, false otherwise.
    A note which is not already played and less than 5s away from current-time should be displayed in canvas."
   (let [distance (note-distance note state)] (and (> distance -1) (< distance 5)))
@@ -121,66 +120,18 @@
     )
   )
 
-(defn should-display [distance]
-  "A note which is not already played and less than 5s away from current-time should be displayed in canvas"
-  (and (> distance -1) (< distance 5))
-  )
-
-(defn calc [note current-time]
-  "The matching y point is at (frame - height) "
-  (let [
-        note-time (j/get note :time)
-        distance (cond
-                   (< current-time 0) (- note-time current-time)
-                   :else (- note-time current-time)
-                   )
-        ]
-
-    ; 0.0123
-    ;(js/console.log "Evaluating note:")
-    ;(js/console.log note)
-    ;(js/console.log "CURRENT_TIME?:")
-    ;(js/console.log current-time)
-    ;(js/console.log "NOTE_TIME?:")
-    ;(js/console.log note-time)
-    ;(js/console.log "DIFF?:")
-    ;(js/console.log distance)
-
-    ; TODO: take out while note-time < current-time, they are past notes
-
-    (cond
-      ; FIXME: Not -, should be a proportion based on frame height to start from top
-      ; distance is %
-      ; 0 => 100%
-      ; 5 => 0 %
-      (should-display distance) (let [percentage (* (/ distance 5.0) 100)
-                                      pitch-midi-number (j/get note :midi) ; MIDI pitch number
-                                      reverse-percentage (- 100 percentage)
-                                      tile-y (* 500 (/ reverse-percentage 100))
-                                      note-duration-ms (j/get note :duration) ; duration ms
-                                      ;adjusted-res (- res 50)
-                                      ]
-                                  (q/rect (tile-x pitch-midi-number) tile-y tile-width (tile-height note-duration-ms))
-                                  )
-      :else nil
-      )
-    ;(js/console.log "~~~~~~~~~~~~~~~~~~~~")
-    ;(q/rect 50 479 20 25)
-    )
-  )
-
 (defn draw [state]
   (let [
-        current-time (get-elapsed-time state)
         notes (get state :notes)
         notes-to-display (take-while (fn [note] (should-display-note note state)) notes)
         ]
     (q/background 255)
     (q/fill 0)
     ;(q/clear)
-    ;(dorun (map (fn [note] (calc note current-time)) notes))
-    ;(js/console.log (count notes-to-display))
-    ;(js/console.log (first notes-to-display))
+    (js/console.log "all-notes")
+    (js/console.log (count notes))
+    (js/console.log "notes-to-display")
+    (js/console.log (count notes-to-display))
     (dorun (map (fn [note] (display-note-rect note state)) notes-to-display))
     ;(q/text (str "Frame rate: " (q/target-frame-rate)) 350 20)
     ;(q/text (str "Frame count: " (/ fps 100)) 350 40)
